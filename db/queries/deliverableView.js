69494c921c5e34d151782694a0fe243f927697cd
lemArts <cash.stramel@gmail.com>
2017-05-13 14:10:01 -0700

    Update deliverableView query to have all necessary returns for front end deliverableView chart and table.

--- db/queries/deliverableView.js
+++ db/queries/deliverableView.js
@@ -2,6 +2,42 @@ const connection = require('../../db/index');
 
 exports.selectDeliverableView = function(req, res, next){
 
+    function getProject() {
+        return new Promise(function(resolve, reject) {
+            connection.query({
+                sql: 'SELECT projects.id, projects.title FROM projects \
+                    INNER JOIN deliverables ON projects.id = deliverables.project_id \
+                    WHERE deliverables.id = ?;',
+                timeout: 40000, //40seconds
+                values: req.params.id
+            }, function(error, results) {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    };
+
+    function getDeliverable() {
+        return new Promise(function(resolve, reject) {
+            connection.query({
+                sql: 'SELECT id, title FROM deliverables \
+                    WHERE deliverables.id = ?;',
+                timeout: 40000, //40seconds
+                values: req.params.id
+            }, function(error, results) {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    };
+
+
     function getSow() {
         return new Promise(function(resolve, reject) {
             connection.query({
@@ -9,7 +45,8 @@ exports.selectDeliverableView = function(req, res, next){
                     INNER JOIN tasks ON sow.task_id = tasks.id \
                     INNER JOIN deliverables ON tasks.deliverable_id = deliverables.id \
                     WHERE deliverables.id = ? \
-                    GROUP BY task_id, yr, mo ASC;',
+                    GROUP BY task_id, yr, mo ASC \
+                    ORDER BY yr, mo, task_id ASC;',
                 timeout: 40000, //40seconds
                 values: req.params.id
             }, function(error, results) {
@@ -25,12 +62,13 @@ exports.selectDeliverableView = function(req, res, next){
     function getAssignedEmployees() {
         return new Promise(function(resolve, reject) {
             connection.query({
-                sql: 'SELECT MONTH(start_date) as mo, YEAR(start_date) as yr, SUM(effort) FROM assignments \
+                sql: 'SELECT SUM(effort) as sum_effort, MONTH(start_date) as mo, YEAR(start_date) as yr FROM assignments \
                     INNER JOIN employees ON assignments.employee_id = employees.id \
                     INNER JOIN tasks ON assignments.task_id = tasks.id \
                     INNER JOIN deliverables ON tasks.deliverable_id = deliverables.id \
                     WHERE deliverables.id = ? \
-                    GROUP BY yr, mo ASC;',
+                    GROUP BY yr, mo ASC \
+                    ORDER BY yr, mo ASC;',
                 timeout: 40000, //40seconds
                 values: req.params.id
             }, function(error, results) {
@@ -46,9 +84,10 @@ exports.selectDeliverableView = function(req, res, next){
     function getTitles() {
         return new Promise(function(resolve, reject) {
             connection.query({
-                sql: 'SELECT tasks.id, tasks.title FROM tasks \
-                    INNER JOIN deliverables ON tasks.deliverable_id = deliverables.id \
-                    WHERE deliverables.id = ?;',
+                sql: 'SELECT tasks.id as id, tasks.title as title FROM tasks \
+                    INNER JOIN deliverables ON tasks.deliverable_id = tasks.id \
+                    WHERE deliverables.id = ? \
+                    GROUP BY id ASC ;',
                 timeout: 40000, //40seconds
                 values: req.params.id
             }, function(error, results) {
@@ -61,14 +100,72 @@ exports.selectDeliverableView = function(req, res, next){
         });
     }
 
-    Promise.all([getTitles(), getSow(), getAssignedEmployees()]).then(function(results) {
-        res.json(results);
+    function getDateRange() {
+        return new Promise(function(resolve, reject) {
+            connection.query({
+                sql: 'SELECT MONTH(start_date) AS mo, YEAR(start_date) as yr FROM sow \
+                    INNER JOIN tasks ON sow.task_id = tasks.id \
+                    INNER JOIN deliverables ON tasks.deliverable_id = deliverables.id \
+                    WHERE deliverables.id = ? \
+                    UNION ALL \
+                    SELECT MONTH(start_date) AS mo, YEAR(start_date) as yr FROM assignments \
+		            INNER JOIN tasks ON assignments.task_id = tasks.id \
+		            INNER JOIN deliverables ON tasks.deliverable_id = deliverables.id \
+		            WHERE deliverables.id = ? \
+                    ORDER BY yr, mo ASC;',
+                timeout: 40000, //40seconds
+                values: [req.params.id, req.params.id]
+            }, function(error, results) {
+                if (error) {
+                    return reject(error);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    }
+
+/*
+    Promise.all([getTitles(), getSow(), getAssignedEmployees(), getDateRange(), getDeliverable()]).then(function(results) {
+        payload = {};
+        payload.tasks = results[0];
+        payload.sow = results[1];
+        payload.assigned_employees = results[2];
+        payload.date_range = [];
+        if (results[3].length > 0) {
+            payload.date_range.push(results[3][0]);
+            payload.date_range.push(results[3][results[3].length - 1]);
+        }
+        payload.project = results[4];
+        res.json(payload);
     }).catch(function(error) {
         res.json({
             error: error
         });
     });
+*/
+
+    Promise.all([getTitles(), getSow(), getAssignedEmployees(), getDateRange(), getDeliverable(), getProject()]).then(function(results) {
+        payload = {};
+        payload.tasks = results[0];
+        payload.sow = results[1];
+        payload.assigned_employees = results[2];
+        payload.date_range = [];
+        if (results[3].length > 0) {
+            payload.date_range.push(results[3][0]);
+            payload.date_range.push(results[3][results[3].length - 1]);
+        }
+        payload.deliverable = results[4];
+        payload.project = results[5];
+        res.json(payload);
+    }).catch(function(error) {
+        res.json({
+            error: error
+        });
+    });
+
 };
+
 
 exports.selectTasksByDeliverables = function(req, res, next){
     connection.query({
